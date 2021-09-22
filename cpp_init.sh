950aabbc9531dda252c96f75ef1a838c2431cb56
Ilya Nikolskiy <aspdaspn@gmail.com>
2021-09-22 20:07:37 +0200

    Enabled C17 Standard

--- cpp_init.sh
+++ cpp_init.sh
@@ -2,20 +2,39 @@
 mkdir src include build
 # Create CMake file
 touch CMakeLists.txt
-echo "CMAKE_MINIMUM_REQUIRED(VERSION 2.8)" > CMakeLists.txt
-echo "PROJECT(project)" >> CMakeLists.txt
-echo "add_executable(project.bin src/main.cpp)" >> CMakeLists.txt
-echo "# set(CMAKE_BUILD_TYPE Debug)" >> CMakeLists.txt
-echo "target_include_directories(project.bin PRIVATE \${CMAKE_CURRENT_SOURCE_DIR}/include)" >> CMakeLists.txt
+cat <<EOT >>CMakeLists.txt
+CMAKE_MINIMUM_REQUIRED(VERSION 3.12)
+
+PROJECT(project)
+add_executable(project.bin src/main.cpp)
+
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED True)
+
+# Enable Database of compiler flags for YCM
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+# Uncomment this for debug
+# set(CMAKE_BUILD_TYPE Debug)
+
+target_include_directories(project.bin PRIVATE \${CMAKE_CURRENT_SOURCE_DIR}/include)
+EOT
+
 # Create source files
 touch include/main.h
-echo "#include <iostream>" > include/main.h
+cat <<EOT >> include/main.h
+#include <iostream>
+EOT
+
 touch src/main.cpp
-echo "#include \"main.h\"" > src/main.cpp
-echo "int main(int argc, char** args) {" >> src/main.cpp
-echo "	std::cout << \"Compiled!\" << std::endl;" >> src/main.cpp
-echo "	return EXIT_SUCCESS;" >> src/main.cpp
-echo "}" >> src/main.cpp
+cat <<EOT >> src/main.cpp
+#include "main.h"
+int main(int argc, char** args) {
+    std::cout << "Compiled!" << std::endl;
+    return EXIT_SUCCESS;
+}
+EOT
+
 # Compile
 cd build
 cmake ../
